76c1ed8b68b4cc6de43cf4a22cb33199756462b5
Makaiya Smith <Makaiya45.cbstl@gmail.com>
2020-02-20 20:43:11 -0600

    Movies in list with title and synopses

---
+++ FlixMAS/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  FlixMAS
+//
+//  Created by Center for Innovation on 2/20/20.
+//  Copyright © 2020 Center for Innovation. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
